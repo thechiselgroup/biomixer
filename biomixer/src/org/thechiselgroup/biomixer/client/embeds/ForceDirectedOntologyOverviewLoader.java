c014a0a26acaebeca792078863309e2a79a2986b
Elena Voyloshnikova <elenavoy@chisel-PC.(none)>
2012-03-15 12:39:06 -0700

    Added edges, highlighting, and zoom to the force directed layout visualization

--- biomixer/src/org/thechiselgroup/biomixer/client/embeds/ForceDirectedOntologyOverviewLoader.java
+++ biomixer/src/org/thechiselgroup/biomixer/client/embeds/ForceDirectedOntologyOverviewLoader.java
@@ -53,9 +53,21 @@ public class ForceDirectedOntologyOverviewLoader implements EmbeddedViewLoader {
 		var w = 1400, h = 1000
 
 		var vis = $wnd.d3.select(div).append("svg:svg").attr("width", w).attr(
-				"height", h).attr("pointer-events", "all").append('svg:g');
+				"height", h).attr("pointer-events", "all").append('svg:g')
+				.call($wnd.d3.behavior.zoom().on("zoom", redraw)).append(
+						'svg:g');
+
+		vis.append('svg:rect').attr('width', w).attr('height', h).attr('fill',
+				'white');
+
+		function redraw() {
+			console.log("here", $wnd.d3.event.translate, $wnd.d3.event.scale);
+			vis.attr("transform", "translate(" + $wnd.d3.event.translate + ")"
+					+ " scale(" + $wnd.d3.event.scale + ")");
+		}
 
 		drawLayout(jsonObject);
+
 		function drawLayout(json) {
 			var force = self.force = $wnd.d3.layout.force().nodes(json.nodes)
 					.links(json.links).gravity(.05).distance(600).charge(-100)
@@ -73,14 +85,14 @@ public class ForceDirectedOntologyOverviewLoader implements EmbeddedViewLoader {
 						return d.target.y;
 					}).style("stroke-width", function(d) {
 						return Math.sqrt(Math.ceil(d.value / 10));
-					});
+					}).style("stroke", "#999").style("stroke-opacity", 0.6);
 
 			link.append("title").text(function(d) {
 				return "Number Of Mappings: " + d.sourceMappings;
 			});
 
-			//link.on("mouseover", highlightLink()).on("mouseout",
-			//		changeColourBack("#496BB0", "#999"));
+			link.on("mouseover", highlightLink()).on("mouseout",
+					changeColourBack("#496BB0", "#999"));
 
 			var node = vis.selectAll("g.node").data(json.nodes).enter().append(
 					"svg:g").attr("class", "node").call(force.drag);
@@ -89,10 +101,11 @@ public class ForceDirectedOntologyOverviewLoader implements EmbeddedViewLoader {
 					.attr("cy", "0px").style("fill", "#496BB0").attr("r",
 							function(d) {
 								return Math.sqrt((d.number) / 10);
-							});
-			//.on("mouseover",
-			//changeColour("#FC6854", "#ff1", "#ff1", .1)).on(
-			//"mouseout", changeColourBack("#496BB0", "#999"));
+							}).style("fill-opacity", 0.7).style("stroke",
+							"#3d3d3d").style("stroke-width", "4px").on(
+							"mouseover",
+							changeColour("#FC6854", "#ff1", "#ff1", .1)).on(
+							"mouseout", changeColourBack("#496BB0", "#999"));
 
 			node.append("title").text(function(d) {
 				return "Number Of Terms: " + d.number;
@@ -123,6 +136,82 @@ public class ForceDirectedOntologyOverviewLoader implements EmbeddedViewLoader {
 					return "translate(" + d.x + "," + d.y + ")";
 				});
 			});
+		}
+		function highlightLink() {
+			return function(d, i) {
+
+				xSourcePos = d.source.x;
+				ySourcePos = d.source.y;
+				xTargetPos = d.target.x;
+				yTargetPos = d.target.y;
+
+				$wnd.d3.selectAll("text").style("opacity", .2).filter(
+						function(g, i) {
+							return g.x == d.source.x || g.y == d.source.y
+									|| g.x == d.target.x || g.y == d.target.y;
+						}).style("opacity", 1);
+
+				$wnd.d3.selectAll("line").style("stroke-opacity", .1);
+				$wnd.d3.selectAll("circle").style("fill-opacity", .1).style(
+						"stroke-opacity", .2).filter(
+						function(g, i) {
+							return g.x == d.source.x || g.y == d.source.y
+									|| g.x == d.target.x || g.y == d.target.y
+						}).style("fill-opacity", 1).style("stroke-opacity", 1);
+				$wnd.d3.select(this).style("stroke-opacity", 1).style("stroke",
+						"#3d3d3d");
+
+			}
+		}
+
+		function changeColour(circleFill, lineFill, circlesFill, opacity) {
+			return function(d, i) {
+
+				xPos = d.x;
+				yPos = d.y;
+
+				$wnd.d3.selectAll("line").style("stroke-opacity", .1);
+				$wnd.d3.selectAll("circle").style("fill-opacity", .1).style(
+						"stroke-opacity", .2);
+
+				$wnd.d3.selectAll("text").style("opacity", .2).filter(
+						function(g, i) {
+							return g.x == d.x
+						}).style("opacity", 1);
+
+				var sourceNode = $wnd.d3.select(this).style("fill", circleFill)
+						.style("fill-opacity", 1).style("stroke-opacity", 1);
+
+				var adjacentLinks = $wnd.d3.selectAll("line").filter(
+						function(d, i) {
+							return d.source.x == xPos && d.source.y == yPos;
+						}).style("stroke-opacity", 1)
+						.style("stroke", "#3d3d3d").each(
+								function(d) {
+									$wnd.d3.selectAll("circle").filter(
+											function(g, i) {
+												return d.target.x == g.x
+														&& d.target.y == g.y;
+											}).style("fill-opacity", 1).style(
+											"stroke-opacity", 1).each(
+											function(d) {
+												$wnd.d3.selectAll("text")
+														.filter(function(g, i) {
+															return g.x == d.x
+														}).style("opacity", 1);
+											});
+								});
+			};
+		}
+
+		function changeColourBack(circleFill, lineFill) {
+			return function(d, i) {
+				$wnd.d3.selectAll("circle").style("fill", circleFill).style(
+						"fill-opacity", .75).style("stroke-opacity", 1);
+				$wnd.d3.selectAll("line").style("stroke", lineFill).style(
+						"stroke-opacity", .75);
+				$wnd.d3.selectAll("text").style("opacity", 1);
+			};
 		}
     }-*/;
 
