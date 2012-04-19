d787d7f1c3918eadf4193d40472636d13c68623c
drusk <rusk.dav@gmail.com>
2012-04-19 11:10:12 -0700

    Added totoe library for JSONPath support

--- biomixer/src/org/thechiselgroup/biomixer/client/services/term/TermTest.java
+++ biomixer/src/org/thechiselgroup/biomixer/client/services/term/TermTest.java
@@ -1,9 +1,14 @@
 package org.thechiselgroup.biomixer.client.services.term;
 
-import org.thechiselgroup.biomixer.client.core.util.UriUtils;
+import name.pehl.totoe.json.client.JsonPath;
+
 import org.thechiselgroup.biomixer.client.services.NcboJsonpRestUrlBuilderFactory;
 import org.thechiselgroup.biomixer.client.workbench.util.url.JsonpUrlFetchService;
 
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
@@ -18,16 +23,16 @@ public class TermTest extends GWTTestCase {
     }
 
     public void testJsonp() {
-        String url = new NcboJsonpRestUrlBuilderFactory()
-                .createUrlBuilder()
-                .uriParameter(
-                        "path",
-                        "/virtual/ontology/1487?conceptid="
-                                + UriUtils
-                                        .encodeURIComponent("http://who.int/bodysystem.owl#BodySystem"))
-                .toString();
-        // String url = new NcboJsonpRestUrlBuilderFactory().createUrlBuilder()
-        // .parameter("path", "%2Fvirtual%2Fontology%2F1487").toString();
+        // String url = new NcboJsonpRestUrlBuilderFactory()
+        // .createUrlBuilder()
+        // .uriParameter(
+        // "path",
+        // "/virtual/ontology/1487?conceptid="
+        // + UriUtils
+        // .encodeURIComponent("http://who.int/bodysystem.owl#BodySystem"))
+        // .toString();
+        String url = new NcboJsonpRestUrlBuilderFactory().createUrlBuilder()
+                .parameter("path", "%2Fvirtual%2Fontology%2F1487").toString();
 
         // &conceptid=http%3A%2F%2Fwho.int%2Fbodysystem.owl%23BodySystem
 
@@ -44,6 +49,18 @@ public class TermTest extends GWTTestCase {
             @Override
             public void onSuccess(String result) {
                 System.out.println("Result:\n" + result);
+                JSONObject jsonObject = JSONParser.parseStrict(result)
+                        .isObject();
+                if (jsonObject == null) {
+                    System.err.println("ERROR");
+                }
+
+                JSONValue select = JsonPath.select(jsonObject,
+                        "$.success.data[0].id");
+                System.out.println("Data:");
+                JSONString string = select.isString();
+                System.out.println(string.stringValue());
+
             }
         });
 
