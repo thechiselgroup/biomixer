b28ea563dce44c15ee7286284901d0698d7b1124
everbeek <eric.l.verbeek@gmail.com>
2013-06-03 16:34:00 -0700

    #270 Made node sizing local normalized on node area (as opposed to node diameter). This appeared best according to Eric, Bo, Cassie and Laura. Doing same for arcs next.

--- biomixer/src/org/thechiselgroup/biomixer/client/visualization_component/graph/rendering/implementation/NodeSizeTransformer.java
+++ biomixer/src/org/thechiselgroup/biomixer/client/visualization_component/graph/rendering/implementation/NodeSizeTransformer.java
@@ -16,13 +16,11 @@ public abstract class NodeSizeTransformer implements
     // 20 * 7 seems too big. Got 20 from other transformers.
     double MAX_ON_SCREEN_SIZE = 20 * 5;
 
-    double MIN_ON_SCREEN_SIZE = 10;
+    double MIN_ON_SCREEN_SIZE = 3;
 
     protected double minRawSize = -1;
 
     protected double maxRawSize = -1;
-
-    protected double rangeRawSize;
 
     private Set<AbstractGraphRenderer> graphRenderingListeners = new HashSet();
 
@@ -45,10 +43,30 @@ public abstract class NodeSizeTransformer implements
         }
 
         // Our factor here is linear. Perhaps we want other functions?
-        double factor = 1.0 - ((this.maxRawSize - rawValue) / this.rangeRawSize);
-        double transformedValue = linearRelativeScaledRangeValue(factor);
-        // transformedValue = logRelativeScaledRangeValue(factor);
+        double factor = this.computeFactorOfRange(rawValue);
+        // double factor = computeFactorOfMax(rawValue);
+
+        double transformedValue = linearAreaRelativeScaledRangeValue(factor);
+
+        // double transformedValue = linearRelativeScaledRangeValue(factor);
+        // double transformedValue = sqRootRelativeScaledRangeValue(factor);
+        // double transformedValue = logRelativeScaledRangeValue(factor);
         return transformedValue;
+    }
+
+    private double computeFactorOfMax(double rawValue) {
+        return rawValue / Math.max(1, this.maxRawSize);
+    }
+
+    private double computeFactorOfRange(double rawValue) {
+        return 1.0 - (this.maxRawSize - rawValue) / this.computeRangeRawSize();
+    }
+
+    private double linearAreaRelativeScaledRangeValue(double factor) {
+        double linearArea = Math.PI * Math.pow(MIN_ON_SCREEN_SIZE, 2) + factor
+                * Math.PI * Math.pow(MAX_ON_SCREEN_SIZE, 2);
+        double diameter = Math.sqrt(linearArea / Math.PI);
+        return diameter;
     }
 
     private double linearRelativeScaledRangeValue(double factor) {
@@ -56,13 +74,27 @@ public abstract class NodeSizeTransformer implements
                 * (MAX_ON_SCREEN_SIZE - MIN_ON_SCREEN_SIZE);
     }
 
-    private double logRelativeScaledRangeValue(double factor) {
+    private double sqRootRelativeScaledRangeValue(double factor) {
         // TODO This is ok, but not great. If the linear one isn't working
         // to satisfaction, try tweaking this one.
         double multiplier = 6;
         return multiplier
                 * Math.sqrt(MIN_ON_SCREEN_SIZE + factor
                         * (MAX_ON_SCREEN_SIZE - MIN_ON_SCREEN_SIZE));
+    }
+
+    private double logRelativeScaledRangeValue(double factor) {
+        // TODO This is ok, but not great. If the linear one isn't working
+        // to satisfaction, try tweaking this one.
+        double multiplier = 6;
+        return multiplier
+                * Math.log(MIN_ON_SCREEN_SIZE + factor
+                        * (MAX_ON_SCREEN_SIZE - MIN_ON_SCREEN_SIZE)
+                        / Math.log(10));
+    }
+
+    private double computeRangeRawSize() {
+        return Math.max(1, maxRawSize - minRawSize);
     }
 
     /**
@@ -80,7 +112,6 @@ public abstract class NodeSizeTransformer implements
         }
         this.minRawSize = minRawSize;
         this.maxRawSize = maxRawSize;
-        this.rangeRawSize = Math.max(1, maxRawSize - minRawSize);
         if (informListeners) {
             informListeners();
         }
