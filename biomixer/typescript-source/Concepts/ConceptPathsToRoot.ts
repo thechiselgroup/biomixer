ea4a5e2c04ea0f5388b16ddf2d8cb004b9bc68c7
everbeek <eric.l.verbeek@gmail.com>
2014-09-29 13:37:47 -0700

    #423 After merge with Elena's work, tree default for path to root works. It needs some improvement with regards to node load order and animation though. This might be tricky.

--- biomixer/typescript-source/Concepts/ConceptPathsToRoot.ts
+++ biomixer/typescript-source/Concepts/ConceptPathsToRoot.ts
@@ -210,8 +210,7 @@ export class ConceptPathsToRoot extends GraphView.BaseGraphView<ConceptGraph.Nod
 		// All of the initial expansions rely ont he expansion set getting the parent node at a slightly delayed time. See each specialized callback
 		// to see where this occurs.
         if(this.visualization === ConceptGraph.PathOptionConstants.pathsToRootConstant){
-              // See issue 423...we want vertical here, but the layout currently misbehaves when the graph is still being populated.
-//            console.log("vertical"); this.setCurrentLayout(this.layouts.runVerticalTreeLayoutLambda());
+            this.setCurrentLayout(this.layouts.runVerticalTreeLayoutLambda());
             this.conceptGraph.fetchPathToRoot(this.centralOntologyAcronym, this.centralConceptUri, expansionSet);
         } else if(this.visualization === ConceptGraph.PathOptionConstants.termNeighborhoodConstant){
             this.conceptGraph.fetchTermNeighborhood(this.centralOntologyAcronym, this.centralConceptUri, expansionSet);
