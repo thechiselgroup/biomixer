c5e033ab2560558a8364b0bf86fc0ed9ef68d89a
everbeek <eric.l.verbeek@gmail.com>
2014-11-21 13:33:50 -0800

    #442 Made the color picker so that it is created and destroyed when the arc is clicked on. This required a separate overlay element, because if the picker anchored object was used to trigger opening, it would close on the same click it was opened. Added a one line instruction to click the sample arcs to change their color.

--- biomixer/typescript-source/Concepts/ConceptEdgeTypeFilter.ts
+++ biomixer/typescript-source/Concepts/ConceptEdgeTypeFilter.ts
@@ -52,7 +52,16 @@ export class ConceptEdgeTypeFilter extends FilterWidget.AbstractFilterWidget imp
                 linkTypeToOntology[d.relationType] = d.relationSpecificToOntologyAcronym;
             }
         );
-
+        
+        // Make instructions for changing colors
+        if(0 === $("#"+"span_color_instructions").length){
+            var inst = $("<span>").attr("id", "span_color_instructions")
+                .text("(Click sample arc for color change)")
+                .addClass("unselectable")
+                .css("font-style", "italic")
+            ;
+            this.filterContainer.append(inst);
+        }
         
         // Add new ones
         $.each(linkTypes, (linkTypeName: string, linkTypeLabel: string) =>
@@ -95,6 +104,7 @@ export class ConceptEdgeTypeFilter extends FilterWidget.AbstractFilterWidget imp
         $("#"+"span_"+this.computeCheckId(this.conceptGraph.relationLabelConstants.mapping)).prependTo(this.filterContainer);
         $("#"+"span_"+this.computeCheckId(this.conceptGraph.relationLabelConstants.composition)).prependTo(this.filterContainer);
         $("#"+"span_"+this.computeCheckId(this.conceptGraph.relationLabelConstants.inheritance)).prependTo(this.filterContainer);
+        $("#"+"span_color_instructions").prependTo(this.filterContainer);
         
         // Keep only those checkboxes for which we looped over a node
         preExistingCheckboxes.not(checkboxesPopulatedOrReUsed).remove();
@@ -142,42 +152,11 @@ export class ConceptEdgeTypeFilter extends FilterWidget.AbstractFilterWidget imp
                 return ConceptEdgeTypeFilter.sampleEdgeClassSansDot
                 +" "+GraphView.BaseGraphView.linkClassSelectorPrefix+relationType
                 +" "+this.graphView.getLinkCssClass(relationType, relationSpecificToOntologyAcronym)
+                +" "+"unselectable"
                 ;
             }
         )
         .attr("id", function(){ return "filter_link_g_"+relationType})
-        .each(function(d: any, i: number){
-                // Get the actual polyline that will go in here
-                var sampleArc = $("#filter_link_marker_"+relationType);
-                // Even though the spectrum color picker will be attached to the svg:g, it works with the line.
-                $(this).spectrum(
-                    {
-                        color: $.stylesheet("."+outerThis.graphView.getLinkCssClass(relationType, relationSpecificToOntologyAcronym)).css("fill"),
-                        change: ()=>{outerThis.updateArcColor($(this), sampleArc, relationType, relationSpecificToOntologyAcronym)},
-                        beforeShow: ()=>{
-                                // var arcColor = $.stylesheet("."+outerThis.graphView.getLinkCssClass(relationType, relationSpecificToOntologyAcronym)).css("fill");
-                                // console.log(arcColor);
-                                // $(this).spectrum("option", "color",arcColor);
-                            },
-                        show: ()=>{ },
-                        hide: ()=>{outerThis.updateArcColor($(this), sampleArc, relationType, relationSpecificToOntologyAcronym)},
-                        showInitial: true,
-                        showPalette: true,
-                        palette: [
-                            ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
-                            ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
-                            // ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"], // To pale
-                            ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
-                            ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
-                            ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
-                            ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
-                            ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
-                        ]
-                    }
-                );
-                return;
-            }
-        )    
         ;
         
         // Make sample marker
@@ -215,6 +194,66 @@ export class ConceptEdgeTypeFilter extends FilterWidget.AbstractFilterWidget imp
         .append("title").text("Click to select color")
         ;
         
+        
+        // Make click handler overlay
+        // In order to have correct clicking behavior combined with a single live
+        // spectrum widget for all possible arcs, I needed to put in an extra svg element
+        // to serve as a proxy for the svg parent. If this g element were ever to receive
+        // a mouse event, the spectrum widget would trigger but then instantly disappear.
+        // because the trigger cannot be the same element as the element spectrum is bound to.
+        d3.select(linkContainer).each(function(d: any, i: number){
+            var spectrumOwner = $(this);
+            var proxyId: string = "filter_link_g_color_box_proxy_"+relationType;
+            var exists = false;
+            
+            d3.select(this).append("svg:rect").attr("id", proxyId)
+            .attr("width", finalTargetXCoordinate).attr("height", 2 * arcHeight)
+            .style("fill-opacity", "0.0").style("stroke-opacity", "0.0")
+            .each(function(){
+                var spectrumActivator = $(this);
+                spectrumActivator.on("click", function(e?){
+                    // Get the actual polyline that will go in here
+                    var sampleArc = $("#filter_link_marker_"+relationType);
+                    if(exists){
+                        return;
+                    }
+                    console.log("creating");
+                    exists = true;
+                    // Even though the spectrum color picker will be attached to the svg:g, it works with the line.
+                    spectrumOwner.spectrum(
+                        {
+                            color: $.stylesheet("."+outerThis.graphView.getLinkCssClass(relationType, relationSpecificToOntologyAcronym)).css("fill"),
+                            change: ()=>{outerThis.updateArcColor(spectrumOwner, sampleArc, relationType, relationSpecificToOntologyAcronym)},
+                            beforeShow: ()=>{ },
+                            show: ()=>{ },
+                            hide: ()=>{
+                                    console.log("hiding");
+                                    outerThis.updateArcColor(spectrumOwner, sampleArc, relationType, relationSpecificToOntologyAcronym);
+                                    spectrumOwner.spectrum("destroy");
+                                    exists = false;
+                                },
+                            showInitial: true,
+                            showPalette: true,
+                            palette: [
+                                ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
+                                ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
+                                // ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"], // To pale
+                                ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
+                                ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
+                                ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
+                                ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
+                                ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+                            ]
+                        }
+                    );
+                    spectrumOwner.spectrum("show");
+                    e.stopPropagation();
+                    return false;
+                });
+            });
+        })
+        ;
+
         return linkContainer;
     }
 
