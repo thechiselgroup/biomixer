3998644e4e0398f401fc41f14f10c3c4ce9c2c2e
everbeek <eric.l.verbeek@gmail.com>
2014-12-03 13:52:40 -0800

    Fix require_config.js to be file relative path.

--- demo1/require_config.js
+++ demo1/require_config.js
@@ -1,7 +1,7 @@
 // Include this on all html pages that include TypeScript stuff.
 
 var require = {
-    	baseUrl: "/d3_graphs/typescript-output/",
+    	baseUrl: "./d3_graphs/typescript-output/",
     	"shim": {
             "jquery.jsonp"  : ["jquery"]
         }
