26522a01b97640bd91c029ce50aa8b876e872a70
everbeek <eric.l.verbeek@gmail.com>
2015-07-09 14:13:24 -0700

    #542 Fixed lock up in IE when we ran tours. Classlist behaviors across browsers are not consistent, and it seems it's not consistent across releases of IE either.

--- biomixer/war/d3_graphs/introjs/biomixer-custom-intro.js
+++ biomixer/war/d3_graphs/introjs/biomixer-custom-intro.js
@@ -453,6 +453,7 @@
 //      showElement.className = showElement.className.replace(/introjs-[a-zA-Z]+/g, '').replace(/^\s+|\s+$/g, ''); // This is a manual trim.
 //        console.log(list);
     	var list = showElement.classList;
+    	var listLength = list.length;
         var j = 0;
     	while(j < list.length){
     		var item = list.item(j);
@@ -460,7 +461,10 @@
     		if(item.indexOf("introjs-") === 0){
     			showElement.classList.remove(item);
     			// so strange, but needed since classList is not an array and may not be copied as arrays normally are
-    			j = 0;
+    			if(list.length !== listLength){
+    				// List has been modified when we modified its source (pointer style)
+    				j = 0;
+    			}
     		}
     		j++
     	}
@@ -473,6 +477,7 @@
       for (var i = fixParents.length - 1; i >= 0; i--) {
 //        fixParents[i].className = fixParents[i].className.replace(/introjs-fixParent/g, '').replace(/^\s+|\s+$/g, '');
     	var list = fixParents[i].classList;
+    	var listLength = list.length;
 //        console.log(list);
         var j = 0;
       	while(j < list.length){
@@ -481,7 +486,10 @@
       		if(item.indexOf("introjs-fixParent") === 0){
       			fixParents[i].classList.remove(item);
     			// so strange, but needed since classList is not an array and may not be copied as arrays normally are
-    			j = 0;
+      			if(list.length !== listLength){
+    				// List has been modified when we modified its source (pointer style)
+    				j = 0;
+      			}
       		}
     		j++
       	}
@@ -901,6 +909,7 @@
         for (var i = fixParents.length - 1; i >= 0; i--) {
 //          fixParents[i].className = fixParents[i].className.replace(/introjs-fixParent/g, '').replace(/^\s+|\s+$/g, '');
         	var list = fixParents[i].classList;
+        	var listLength = list.length;
 //            console.log(list);
             var j = 0;
         	while( j < list.length){
@@ -909,7 +918,10 @@
         		if(item.indexOf("introjs-fixParent") === 0){
         			fixParents[i].classList.remove(item);
         			// so strange, but needed since classList is not an array and may not be copied as arrays normally are
-        			j = 0;
+        			if(list.length !== listLength){
+        				// List has been modified when we modified its source (pointer style)
+        				j = 0;
+        			}
         		}
         		j++
         	}
@@ -922,6 +934,7 @@
       if(oldShowElement){
 //      oldShowElement.className = oldShowElement.className.replace(/introjs-[a-zA-Z]+/g, '').replace(/^\s+|\s+$/g, '');
       var list = oldShowElement.classList;
+      var listLength = list.length;
 //      console.log(list);
       var j = 0;
 	  while( j < list.length){
@@ -929,10 +942,13 @@
 //		  console.log("4"+item);
 		  if(item.indexOf("introjs-") === 0){
 			  oldShowElement.classList.remove(item);
-  			// so strange, but needed since classList is not an array and may not be copied as arrays normally are
-  			j = 0;
+  			  // so strange, but needed since classList is not an array and may not be copied as arrays normally are
+			  if(list.length !== listLength){
+				// List has been modified when we modified its source (pointer style)
+	  			j = 0;
+			  }
 		  }
-  		j++
+		  j++
 	  }
 //	  console.log(oldShowElement.classList);
       }
